caf8ccd0f56af4dcb7bbb5bbedfc02fe47d3b8af
Luke Olney <lukeolney@gmail.com>
2018-01-03 23:13:13 -0800

    Updated to work with current version of MelonJS

--- pathFinding.js
+++ pathFinding.js
@@ -1,32 +1,49 @@
-(function () {
-    // ensure that me.pathFinding is defined
-    me.pathFinding = me.pathFinding || {};
-    me.pathFinding = me.plugin.Base.extend(
+(function ($) {
+    var pathFindingInstance = function() {};
+
+    pathFindingInstance.prototype.init = function(width, height, type, length, loc) {
+        type = type || "JumpPointFinder";
+        length = length || 1;
+        loc = loc || "lib/plugins/pathFinding/";
+
+        this.name = "me.pathFinding";
+
+        this.worker = new Worker(loc + "jobWorker.js");
+
+        this.postMessage({  name: "init", 
+                            data:{
+                                type: type, width: width, height: height, length: length
+                            }
+                        });
+    };
+
+    pathFindingInstance.prototype.addListener = function(listener) {
+            this.worker.addEventListener('message', listener, false);
+    };
+
+
+
+    pathFindingInstance.prototype.postMessage = function(object){
+            this.worker.postMessage(object);
+    };
+
+    pathFindingPlugin = me.plugin.Base.extend(
     {
 
-        init : function (width, height, type, length, location) {
-            type = type || "JumpPointFinder";
-            length = length || 1;
-            location = location || "lib/plugins/pathFinding/";
+        init : function (width, height, type, length, loc) {
             this._super(me.plugin.Base, "init");
-
+            this.GUID = "pathFinding-"+me.utils.createGUID();
             this.name = "me.pathFinding";
+            this.isPersistent = true;
 
-            this.worker = new Worker(location + "jobWorker.js");
-            this.worker.addEventListener('message', this.workerListener, false);
-            this.postMessage({  name: "init", 
-                                data:{
-                                    type: type, width: width, height: height, length: length
-                                }
-                            });
+            me.event.subscribe(me.event.LEVEL_LOADED, function(){
+                if (me.pathFinding != null) {
+                    // alternatively, just update the nodes?
+                    me.pathFinding = null;
+                }
+                me.pathFinding = new pathFindingInstance();
+                me.pathFinding.init(width, height, type, length, loc);
+            });
         },
-
-        workerListener : function (e) {
-            var object = e.data;
-            console.log(object);
-        },
-        postMessage : function(object){
-            this.worker.postMessage(object);
-        }
     });
-})();
+})(window);
